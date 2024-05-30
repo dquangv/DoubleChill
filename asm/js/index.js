1ee1e2146504694932345c98af4044a3f4fdc5ef
Quang Vu <dquangvu7998@gmail.com>
2024-05-30 23:29:23 +0700

    upload tour data - file json

--- asm/js/index.js
+++ asm/js/index.js
@@ -1,7 +1,7 @@
 var app = angular.module("myapp", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/", { templateUrl: "asm/html/home.html", controller: "myCtrl" })
+        .when("/", { templateUrl: "asm/html/home.html", controller: "homeCtrl" })
         .when("/aboutus", { templateUrl: "asm/html/aboutus.html", controller: "myCtrl" })
         .when("/domestic", { templateUrl: "asm/html/domestic.html", controller: "myCtrl" })
         .when("/foreign", { templateUrl: "asm/html/foreign.html", controller: "myCtrl" })
@@ -10,4 +10,13 @@ app.config(function ($routeProvider) {
         .when("/signup", { templateUrl: "asm/html/signUp.html", controller: "myCtrl" })
         .when("/cart", { templateUrl: "asm/html/cart.html", controller: "myCtrl" })
         .otherwise({ templateUrl: "asm/html/home.html", controller: "myCtrl" })
-})
+});
+
+app.controller("homeCtrl", function ($scope, $rootScope, $routeParams, $http) {
+    $scope.tours = [];
+    $http.get("http://localhost:3000/tours").then(function (reponse) {
+        $scope.tours = reponse.data;
+        console.log($scope.tours);
+        $scope.detailPro = $scope.tours.find(item => item.id == $routeParams.id);
+    });
+});
