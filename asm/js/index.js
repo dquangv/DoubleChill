98e16dbfea2876e2ee5c3d080f3babd9c4df3795
Quang Vu <dquangvu7998@gmail.com>
2024-05-31 22:11:01 +0700

    edit tour of pages (angular - ngRepeat)

--- asm/js/index.js
+++ asm/js/index.js
@@ -3,12 +3,12 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when("/", { templateUrl: "asm/html/home.html", controller: "homeCtrl" })
         .when("/aboutus", { templateUrl: "asm/html/aboutus.html", controller: "myCtrl" })
-        .when("/domestic", { templateUrl: "asm/html/domestic.html", controller: "myCtrl" })
-        .when("/foreign", { templateUrl: "asm/html/foreign.html", controller: "myCtrl" })
+        .when("/domestic", { templateUrl: "asm/html/domestic.html", controller: "domesticCtrl" })
+        .when("/foreign", { templateUrl: "asm/html/foreign.html", controller: "foreignCtrl" })
         .when("/contact", { templateUrl: "asm/html/contact.html", controller: "myCtrl" })
         .when("/login", { templateUrl: "asm/html/login.html", controller: "myCtrl" })
         .when("/signup", { templateUrl: "asm/html/signUp.html", controller: "myCtrl" })
-        .when("/cart", { templateUrl: "asm/html/cart.html", controller: "myCtrl" })
+        .when("/cart/:id", { templateUrl: "asm/html/cart.html", controller: "homeCtrl" })
         .otherwise({ templateUrl: "asm/html/home.html", controller: "myCtrl" })
 });
 
@@ -16,7 +16,25 @@ app.controller("homeCtrl", function ($scope, $rootScope, $routeParams, $http) {
     $scope.tours = [];
     $http.get("http://localhost:3000/tours").then(function (reponse) {
         $scope.tours = reponse.data;
-        console.log($scope.tours);
+        // console.log($scope.tours);
+        $scope.detailPro = $scope.tours.find(item => item.id == $routeParams.id);
+    });
+});
+
+app.controller("domesticCtrl", function ($scope, $rootScope, $routeParams, $http) {
+    $scope.tours = [];
+    $http.get("http://localhost:3000/tours").then(function (reponse) {
+        $scope.tours = reponse.data;
+        // console.log($scope.tours);
+        $scope.detailPro = $scope.tours.find(item => item.id == $routeParams.id);
+    });
+});
+
+app.controller("foreignCtrl", function ($scope, $rootScope, $routeParams, $http) {
+    $scope.tours = [];
+    $http.get("http://localhost:3000/tours").then(function (reponse) {
+        $scope.tours = reponse.data;
+        // console.log($scope.tours);
         $scope.detailPro = $scope.tours.find(item => item.id == $routeParams.id);
     });
 });
